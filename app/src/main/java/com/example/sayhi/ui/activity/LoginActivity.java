68e41377af2dfe2c5dbe27f5f7980df540aaab6b
SuRakib <rakibul15-10976@diu.edu.bd>
2023-03-05 11:04:11 +0600

    Eighth Commit*(Image in LoginActivity)

--- app/src/main/java/com/example/sayhi/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/sayhi/ui/activity/LoginActivity.java
@@ -9,10 +9,18 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.sayhi.databinding.ActivityLoginBinding;
+import com.example.sayhi.ui.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -53,9 +61,30 @@ public class LoginActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if(task.isSuccessful()){
+                    FirebaseUser firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+                    FirebaseDatabase.getInstance().getReference("user").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                         User user= snapshot.getValue(User.class);
+                         if (Objects.requireNonNull(user).getProfileImage().equals("img")){
 
-                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                    finish();
+                             startActivity(new Intent(getApplicationContext(),ImageUploadActivity.class));
+
+                             finish();
+
+                         }else{
+
+                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                             finish();
+                         }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
 
 
                 }else{
